857f23b613b4a2a359908b88881787e85e5f8b64
mdmaerz2 <mdmaerz@gmail.com>
2025-11-17 12:37:48 -0800

    Update 4_tcr_preprocessing_v3.R

--- ivbcg_scRNAseq/4_tcr_preprocessing_v3.R
+++ ivbcg_scRNAseq/4_tcr_preprocessing_v3.R
@@ -10,7 +10,7 @@ library(devtools)
 library(scRepertoire)
 library(tibble)
 
-#Read in the filtered_contig_annotations files and the T cell seurat objects
+# Read in the filtered_contig_annotations files and the T cell seurat objects
 dg0d_t <- read.csv(file = 'Tube1_vdj_v3.0.2_DG0D/filtered_contig_annotations.csv')
 dg0h_t <- read.csv(file = 'Tube1_vdj_v3.0.2_DG0H/filtered_contig_annotations.csv')
 v113_t <- read.csv(file = 'Tube1_vdj_v3.0.2_V113/filtered_contig_annotations.csv')
@@ -22,7 +22,7 @@ v113_g <- readRDS(file = 'out/seurat_objects/v113_T.rds')
 v015_g <- readRDS(file = 'out/seurat_objects/v015_T.rds')
 
 
-#Filter out all TCR barcodes not present in the _analysis.rds files. This maintains only the barcodes that passed GEX QC.
+# Filter out all TCR barcodes not present in the RDS files. This maintains only the barcodes that passed GEX QC.
 filter_tcr <- function(gex_csv, tcr_csv) { 
   gex_bc <- colnames(gex_csv) %>% as.data.frame()
   colnames(gex_bc) <- "barcode"
@@ -73,13 +73,26 @@ v113_qcd <- v113_qcd %>% filter(umis >2, reads >20)
 v015_qcd <- v015_qcd %>% filter(umis >2, reads >20)
 
 
-#Use scRepertoire to define a single TRD and TRG per chain
+# Use scRepertoire to define a single TRD and TRG per chain
+# Generate lists of (1) paired-chain TCRs and (2) barcodes from which only one productive chain was measured
 contig_list <- list(dg0d_qcd, dg0h_qcd, v113_qcd, v015_qcd)
-combined <- combineTCR(contig_list, samples = c("dg0d", "dg0h", "v113", "v015"), ID = NULL, cells = "T-GD", removeNA = TRUE, removeMulti = FALSE, filterMulti = TRUE) #Keeps only T cells with paired-chain data
-combined_sch <- combineTCR(contig_list, samples = c("dg0d", "dg0h", "v113", "v015"), ID = NULL, cells = "T-GD", removeNA = FALSE, removeMulti = FALSE, filterMulti = TRUE) #Keeps T cells with at least one chain
+combined <- combineTCR(contig_list, 
+                       samples = c("dg0d", "dg0h", "v113", "v015"), 
+                       ID = NULL, 
+                       cells = "T-GD", 
+                       removeNA = TRUE, 
+                       removeMulti = FALSE, 
+                       filterMulti = TRUE) #Keeps only T cells with paired-chain data
+combined_sch <- combineTCR(contig_list, 
+                           samples = c("dg0d", "dg0h", "v113", "v015"), 
+                           ID = NULL, 
+                           cells = "T-GD", 
+                           removeNA = FALSE, 
+                           removeMulti = FALSE, 
+                           filterMulti = TRUE) #Keeps T cells with at least one chain
 
 
-#Export the combined TCR dfs
+#Export the combined TCR dataframes
 dg0d_pch <- combined$dg0d
 dg0d_sch <- combined_sch$dg0d
 write.csv(dg0d_pch, file= "out/tcr/dg0d_tcr_paired.csv")
@@ -133,142 +146,3 @@ v015_sch <- subset(v015_sch, select=-c(barcode)) %>% rename(barcode = barcode2) 
 v015_sch <- v015_sch$barcode %>% as.data.frame()
 names(v015_sch) <- c("barcode")
 write.csv(v015_sch, "out/tcr/v015_gd_bc.csv")
-
-
-
-
-###########
-## NOTES ##
-###########
-
-## Generate new lists to allow comparisons between samples within a single PTID ##
-#DG0D
-dg0d_t <- combined$dg0d
-
-barcodes <- c(dg0d_t$barcode) #Store the barcodes
-barcodes2 <- gsub("dg0d_", "", barcodes, fixed = TRUE) #Remove the prefix from the stored barcodes
-dg0d_t$barcode2 <- c(barcodes2) #Replace the barcodes column with the cleaned-up barcodes
-dg0d_t <- subset(dg0d_t, select=-c(barcode)) %>% rename(barcode = barcode2) #Drop the replicate column and rename
-
-dg0d_g
-dg0d_anno <- FetchData(object = dg0d_g, vars = c("Tissue", "Timepoint"))
-dg0d_anno <- tibble::rownames_to_column(dg0d_anno, "barcode")
-
-dg0d_t <- merge(dg0d_t, dg0d_anno, by = "barcode")
-dg0d_t$sample <- paste(dg0d_t$Tissue, dg0d_t$Timepoint, sep = "_")
-dg0d_split <- dg0d_t %>% split(dg0d_t, f = dg0d_t$sample)
-
-
-#DG0H
-dg0h_t <- combined$dg0h
-
-barcodes <- c(dg0h_t$barcode) #Store the barcodes
-barcodes2 <- gsub("dg0h_", "", barcodes, fixed = TRUE) #Remove the prefix from the stored barcodes
-dg0h_t$barcode2 <- c(barcodes2) #Replace the barcodes column with the cleaned-up barcodes
-dg0h_t <- subset(dg0h_t, select=-c(barcode)) %>% rename(barcode = barcode2) #Drop the replicate column and rename
-
-dg0h_anno <- FetchData(object = dg0h_g, vars = c("Tissue", "Timepoint"))
-dg0h_anno <- tibble::rownames_to_column(dg0h_anno, "barcode")
-
-dg0h_t <- merge(dg0h_t, dg0h_anno, by = "barcode")
-dg0h_t$sample <- paste(dg0h_t$Tissue, dg0h_t$Timepoint, sep = "_")
-dg0h_split <- dg0h_t %>% split(dg0h_t, f = dg0h_t$sample)
-
-
-#V113
-v113_t <- combined$v113
-
-barcodes <- c(v113_t$barcode) #Store the barcodes
-barcodes2 <- gsub("v113_", "", barcodes, fixed = TRUE) #Remove the prefix from the stored barcodes
-v113_t$barcode2 <- c(barcodes2) #Replace the barcodes column with the cleaned-up barcodes
-v113_t <- subset(v113_t, select=-c(barcode)) %>% rename(barcode = barcode2) #Drop the replicate column and rename
-
-v113_anno <- FetchData(object = v113_g, vars = c("Tissue", "Timepoint"))
-v113_anno <- tibble::rownames_to_column(v113_anno, "barcode")
-
-v113_t <- merge(v113_t, v113_anno, by = "barcode")
-v113_t$sample <- paste(v113_t$Tissue, v113_t$Timepoint, sep = "_")
-v113_split <- v113_t %>% split(v113_t, f = v113_t$sample)
-
-
-#V015
-v015_t <- combined$v015
-
-barcodes <- c(v015_t$barcode) #Store the barcodes
-barcodes2 <- gsub("v015_", "", barcodes, fixed = TRUE) #Remove the prefix from the stored barcodes
-v015_t$barcode2 <- c(barcodes2) #Replace the barcodes column with the cleaned-up barcodes
-v015_t <- subset(v015_t, select=-c(barcode)) %>% rename(barcode = barcode2) #Drop the replicate column and rename
-
-v015_anno <- FetchData(object = v015_g, vars = c("Tissue", "Timepoint"))
-v015_anno <- tibble::rownames_to_column(v015_anno, "barcode")
-
-v015_t <- merge(v015_t, v015_anno, by = "barcode")
-v015_t$sample <- paste(v015_t$Tissue, v015_t$Timepoint, sep = "_")
-v015_split <- v015_t %>% split(v015_t, f = v015_t$sample)
-
-
-## Analyze the split TCR data using scRepertoire ##
-
-#DG0D
-quantContig(dg0d_split, cloneCall="nt", scale = T)
-abundanceContig(dg0d_split, cloneCall = "nt", scale = T)
-lengthContig(dg0d_split, cloneCall = "nt", chain = "both", scale = T)
-compareClonotypes(dg0d_split, 
-                  chain = "both", 
-                  cloneCall = "aa", 
-                  samples = c("PBMC_Week_0", "PBMC_Week_4", "PBMC_Week_8"), 
-                  graph = "alluvial")
-clonalProportion(dg0d_split, cloneCall = "nt", 
-                  split = c(1, 10, 1e+05))
-
-#DG0H
-quantContig(dg0h_split, cloneCall="nt", scale = T)
-abundanceContig(dg0h_split, cloneCall = "nt", scale = T)
-lengthContig(dg0h_split, cloneCall = "nt", chain = "both", scale = T)
-compareClonotypes(dg0h_split,
-                  numbers = 20,
-                  chain = "both", 
-                  cloneCall = "aa", 
-                  samples = c("PBMC_Week_0", "PBMC_Week_4", "PBMC_Week_8"), 
-                  graph = "alluvial")
-clonalProportion(dg0h_split, cloneCall = "nt", 
-                 split = c(1, 10, 1e+05))
-
-#V113
-quantContig(v113_split, cloneCall="nt", scale = T)
-abundanceContig(v113_split, cloneCall = "nt", scale = T)
-lengthContig(v113_split, cloneCall = "nt", chain = "both", scale = T)
-compareClonotypes(v113_split,
-                  numbers = 20,
-                  chain = "both", 
-                  cloneCall = "aa", 
-                  samples = c("PBMC_Week_0", "PBMC_Week_4", "PBMC_Week_8"), 
-                  graph = "alluvial")
-clonalProportion(v113_split, cloneCall = "nt", 
-                 split = c(1, 10, 1e+05))
-
-#V015
-quantContig(v015_split, cloneCall="nt", scale = T)
-abundanceContig(v015_split, cloneCall = "nt", scale = T)
-lengthContig(v015_split, cloneCall = "nt", chain = "both", scale = T)
-compareClonotypes(v015_split,
-                  numbers = 20,
-                  chain = "both", 
-                  cloneCall = "aa", 
-                  samples = c("PBMC_Week_0", "PBMC_Week_4", "PBMC_Week_8"), 
-                  graph = "alluvial")
-clonalProportion(v015_split, cloneCall = "nt", 
-                 split = c(1, 10, 1e+05))
-
-
-#Measure the proportion of barcodes with >2 contigs per chain
-dg0d_rv <- dg0d_qcd %>% filter(!grepl("contig_1", contig_id)) %>% filter(!grepl("contig_2", contig_id))
-dg0d_rv_bc <- dg0d_rv$barcode %>% as.data.frame()
-colnames(dg0d_rv_bc) <- "barcode"
-check1 <- merge(x = dg0d_qcd, y = dg0d_rv_bc, by = "barcode")
-
-v113_rv <- v113_qcd %>% filter(!grepl("contig_1", contig_id)) %>% filter(!grepl("contig_2", contig_id))
-v113_rv_bc <- v113_rv$barcode %>% as.data.frame()
-colnames(v113_rv_bc) <- "barcode"
-check2 <- merge(x = v113_qcd, y = v113_rv_bc, by = "barcode")
-check2 <- check2 %>% distinct()
